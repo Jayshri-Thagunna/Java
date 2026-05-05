8b95b6868c2f6204fced1da9608606945d047fed
Jayshri-Thagunna <thagunnajayshri02@gmail.com>
2026-05-05 11:52:08 +0530

    classes in java

---
+++ practical/book.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Book {
+    String title;
+    String author;
+    int pages;
+
+    void displayInfo() {
+        System.out.println("Book: " + title + " by " + author + " (" + pages + " pages)");
+    }
+}
+
+public class book {
+    public static void main(String[] args) {
+        System.out.println("Class implemented successfully.");
+    }
+}
